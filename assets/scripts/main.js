4dfe9b7e7b31cc2ef3e0f3f7dbe85586d6e4ad75
Luigi_Bross <cursosinformatica9102@gmail.com>
2022-02-02 17:06:20 -0300

    Finalizado parcialmente Page Portifolio

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -70,3 +70,20 @@ tabs.forEach(tab => {
 
   });
 });
+
+
+
+// Portfolio Swipper
+let swiper = new Swiper(".portfolio__container", {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+});
